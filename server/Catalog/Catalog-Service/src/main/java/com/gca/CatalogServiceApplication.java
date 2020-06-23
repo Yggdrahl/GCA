b91599fcfefb163d22cf36347b14a0b9d18b2286
ArmenGyunashyan <Armenien121>
2020-06-23 17:49:52 +0200

    enable CORS

--- server/Catalog/Catalog-Service/src/main/java/com/gca/CatalogServiceApplication.java
+++ server/Catalog/Catalog-Service/src/main/java/com/gca/CatalogServiceApplication.java
@@ -2,10 +2,12 @@ package com.gca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class CatalogServiceApplication {
-
+	@Bean
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
