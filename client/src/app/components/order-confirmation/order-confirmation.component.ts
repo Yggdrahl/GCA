358318837854a3d34917c15b9958aa2f63fefc4c
ArmenGyunashyan <Armenien121>
2020-06-26 17:54:35 +0200

    Checkout done, added switch() to month and year in Order.java, so the output is correct and to post the Order you now need at least Month and Year, otherwise the post will not start (still work in progress)

--- client/src/app/components/order-confirmation/order-confirmation.component.ts
+++ client/src/app/components/order-confirmation/order-confirmation.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-order-confirmation',
@@ -7,11 +8,29 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class OrderConfirmationComponent implements OnInit {
 
-  constructor() {
+
+  orderNumber;
+  order;
+  urlOrder = 'http://localhost:8083/orders';
+
+  constructor(private http: HttpClient) {
+    if(this.orderNumber != null){
+      this.getOrder();
+    }
   }
 
 
   ngOnInit(): void {
   }
 
+  setOrderNumber(responseNumber){
+    this.orderNumber = responseNumber;
+  }
+
+  getOrder() {
+      this.urlOrder = this.urlOrder + "/" + this.orderNumber;
+      this.http.get(this.urlOrder).subscribe((order) => {
+        this.order = JSON.parse(JSON.stringify(order));
+      });
+  }
 }
--- server/Checkout-Service/Checkout-Service/src/main/java/com/gca/order/Order.java
+++ server/Checkout-Service/Checkout-Service/src/main/java/com/gca/order/Order.java
@@ -32,10 +32,66 @@ public class Order {
 		this.street = street;
 		this.city = city;
 		this.state = state;
-		this.country = country;
-		this.month = month;
+		this.country = country; 
+		switch(month) {
+		case "1":
+			this.month = "January";
+			break;
+		case "2":
+			this.month = "February";
+			break;
+		case "3":
+			this.month = "March";
+			break;
+		case "4":
+			this.month = "April";
+			break;
+		case "5":
+			this.month = "May";
+			break;
+		case "6":
+			this.month = "June";
+			break;
+		case "7":
+			this.month = "July";
+			break;
+		case "8":
+			this.month = "August";
+			break;
+		case "9":
+			this.month = "September";
+			break;
+		case "10":
+			this.month = "October";
+			break;
+		case "11":
+			this.month = "November";
+			break;
+		case "12":
+			this.month = "December";
+			break;
+		}
 		this.cvv = cvv;
-		this.year = year;
+		switch (year) {
+		case 1:
+			this.year = 2020;
+			break;
+		case 2:
+			this.year = 2021;
+			break;
+		case 3:
+			this.year = 2022;
+			break;
+		case 4:
+			this.year = 2023;
+			break;
+		case 5:
+			this.year = 2024;
+			break;
+		case 6: 
+			this.year = 2025;
+			break;
+		}
 		this.zip = zip;
 		this.creditcard = creditcard;
 		
