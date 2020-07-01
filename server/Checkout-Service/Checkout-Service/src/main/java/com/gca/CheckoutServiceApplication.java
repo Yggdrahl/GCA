b2dceea2b653ebac8ca02c40f46ae1c269b12639
Yggdrahl <denniskra159@web.de>
2020-07-01 04:44:22 +0200

    Added swagger to all services. Documentation URL: "localhost:port/swagger-ui.html"

--- server/Checkout-Service/Checkout-Service/src/main/java/com/gca/CheckoutServiceApplication.java
+++ server/Checkout-Service/Checkout-Service/src/main/java/com/gca/CheckoutServiceApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrix
+@EnableSwagger2
 public class CheckoutServiceApplication {
 
 	public static void main(String[] args) {
